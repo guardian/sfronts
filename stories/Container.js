c5bfa5837c092d8809e08bd58da2ca9063e027e1
Laura buns <lawr@lauragonzalez.cc>
2019-04-17 11:31:24 +0100

    gridlinerz

--- stories/Container.js
+++ stories/Container.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { css, cx } from 'emotion';
 import { palette } from '@guardian/pasteup/palette';
+import { boolean } from '@storybook/addon-knobs';
 
 const anchors = ['left', 'right', 'top', 'bottom'];
+const textAnchors = ['start', 'center', 'end'];
 
 const metrics = {
 	gutter: 10,
@@ -11,33 +13,37 @@ const metrics = {
 
 const Text = ({ children, className }) => (
 	<div className={className}>
-		<h2>{children}</h2>
+		<h2 style={{ margin: 0 }}>{children}</h2>
 	</div>
 );
 
-const getFlexBoxForAnchor = anchor => {
-	if (anchor === 'right')
+const getFlexForTextAnchor = textAnchor => {
+	if (textAnchor === 'start')
 		return {
-			justifyContent: 'flex-end',
+			alignSelf: 'flex-start',
 		};
+	if (textAnchor === 'end')
+		return {
+			alignSelf: 'flex-end',
+		};
+};
+
+const getFlexBoxForAnchor = anchor => {
 	if (anchor === 'left')
 		return {
-			justifyContent: 'flex-end',
 			flexDirection: 'row-reverse',
 		};
 	if (anchor === 'top')
 		return {
-			justifyContent: 'flex-end',
 			flexDirection: 'column-reverse',
 		};
 	if (anchor === 'bottom')
 		return {
-			justifyContent: 'flex-end',
 			flexDirection: 'column',
 		};
 };
 
-const styles = ({ anchor }) =>
+const styles = ({ anchor, gridlines }) =>
 	css({
 		overflow: 'hidden',
 		background: palette.brand.main,
@@ -45,29 +51,53 @@ const styles = ({ anchor }) =>
 		display: 'flex',
 		alignItems: 'center',
 		padding: `${metrics.baseline}px ${metrics.gutter}px`,
+		justifyContent: 'flex-end',
 		...getFlexBoxForAnchor(anchor),
+		...(gridlines
+			? {
+					'& > *': {
+						boxShadow: 'inset 0 0 0 1px rgba(255,255,255,.25)',
+					},
+			  }
+			: {}),
 	});
 
-const Container = ({ anchor, split, scale, padding }) => (
-	<div className={styles({ anchor, split })}>
+const Container = ({
+	anchor,
+	textAnchor,
+	textWidth,
+	split,
+	scale,
+	padding,
+	gridlines,
+}) => (
+	<div className={styles({ anchor, gridlines })}>
 		<div
 			className={css({
 				flex: `1 1 0`,
-				transform: `scale(${scale})`,
-				padding,
+				transform: `scale(${scale}) translateX(${padding[0]}%) translateY(${
+					padding[1]
+				}%)`,
 			})}
 		>
 			<img src="http://pngimg.com/uploads/avocado/avocado_PNG15489.png" />
 		</div>
-		<Text
+		<div
 			className={css({
 				flex: `0 0 ${split}%`,
+				...getFlexForTextAnchor(textAnchor),
 			})}
 		>
-			Avocado thief on the loose
-		</Text>
+			<Text
+				className={css({
+					width: textWidth + '%',
+				})}
+			>
+				Avocado thief on the loose
+			</Text>
+		</div>
 	</div>
 );
 
-export { anchors };
+export { anchors, textAnchors };
 export default Container;
--- stories/index.stories.js
+++ stories/index.stories.js
@@ -4,10 +4,10 @@ import { css, cx } from 'emotion';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
-import { withKnobs, text, select, number } from '@storybook/addon-knobs';
+import { withKnobs, select, number, boolean } from '@storybook/addon-knobs';
 
 import { Button, Welcome } from '@storybook/react/demo';
-import Container, { anchors } from './Container';
+import Container, { anchors, textAnchors } from './Container';
 
 storiesOf('Container', module)
 	.addDecorator(withKnobs)
@@ -23,10 +23,31 @@ storiesOf('Container', module)
 			})}
 		>
 			<Container
-				split={number('Split', 50)}
-				anchor={select('Anchor', anchors)}
-				scale={number('Scale', 1)}
-				padding={number('Padding', 0)}
+				gridlines={boolean('Gridlines', true)}
+				split={number('Split', 50, { range: true, min: 10, max: 90 })}
+				textWidth={number('Text width', 50, { range: true, min: 0, max: 200 })}
+				anchor={select('Anchor', anchors, anchors[0])}
+				textAnchor={select('Text anchor', textAnchors, textAnchors[1])}
+				scale={number('Scale', 1, {
+					range: true,
+					min: 0,
+					max: 2,
+					step: 0.01,
+				})}
+				padding={[
+					number('Image translate X', 0, {
+						range: true,
+						min: -100,
+						max: 100,
+						step: 1,
+					}),
+					number('Image translate Y', 0, {
+						range: true,
+						min: -100,
+						max: 100,
+						step: 1,
+					}),
+				]}
 			/>
 		</div>
 	));
